b7bf8e49a4013c773374d19affc4744246d2bb7d
Jacob Piland <piland.jacob@gmail.com>
2021-07-15 18:15:39 -0500

    migrate application db and add instances to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Pet.destroy_all
 Shelter.destroy_all
+AdoptionApplication.destroy_all
 
 @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 @pet_1 = @shelter.pets.create!(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow')
 @pet_2 = @shelter.pets.create!(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe')
 @pet_3 = @shelter.pets.create!(adoptable: false, age: 4, breed: 'chihuahua', name: 'Elle')
+
+@applicant_1 = AdoptionApplication.create!(name: 'John John', street_address: '123 Street', city: 'Denver', state: 'CO', zip_code: 80014, description: 'I have lots of free time and a huge yard!', pets_applied_for: "#{@pet_1.name}", status: 'Pending')
+@applicant_2 = AdoptionApplication.create!(name: 'Dan Dan', street_address: 'Fifth Street', city: 'Tulsa', state: 'OK', zip_code: 74105, description: 'I love animals and Im rich', pets_applied_for: "#{@pet_2.name}", status: 'Pending')
