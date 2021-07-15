21fdf35f5fbdeba68941256353001622efb8f659
Jacob Piland <piland.jacob@gmail.com>
2021-07-15 18:31:16 -0500

    add routes, tests, and method for app show

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,6 @@ Rails.application.routes.draw do
   post '/veterinary_offices/:veterinary_office_id/veterinarians', to: 'veterinarians#create'
 
   get '/applications', to: 'adoption_applications#index'
-
-
+  get '/applications/:id', to: 'adoption_applications#show'
+  post '/applications', to: 'adoption_applications#create'
 end
--- spec/features/adoption_applications/show_spec.rb
+++ spec/features/adoption_applications/show_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe 'the application show' do
-  it 'shows the application and all its attributes' do
-    
+RSpec.describe 'the application show page' do
+  before :each do
+    @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = @shelter.pets.create!(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow')
+    @pet_2 = @shelter.pets.create!(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe')
+    @applicant_1 = AdoptionApplication.create!(name: 'John John', street_address: '123 Street', city: 'Denver', state: 'CO', zip_code: 80014, description: 'I have lots of free time and a huge yard!', pets_applied_for: "#{@pet_1.name}", status: 'Pending')
+    @applicant_2 = AdoptionApplication.create!(name: 'Dan Dan', street_address: 'Fifth Street', city: 'Tulsa', state: 'OK', zip_code: 74105, description: 'I love animals and Im rich', pets_applied_for: "#{@pet_2.name}", status: 'Pending')
 
+    visit "/applications/#{@applicant_1.id}"
   end
+
+  it 'can show specific information about that application' do
+
+    expect(page).to have_content(@applicant_1.name)
+    expect(page).to have_content(@applicant_1.street_address)
+    expect(page).to have_content(@applicant_1.city)
+    expect(page).to have_content(@applicant_1.state)
+    expect(page).to have_content(@applicant_1.zip_code)
+    expect(page).to have_content(@applicant_1.description)
+    expect(page).to have_content(@applicant_1.status)
+  end
+
+  it 'does not show specific information of other applications' do
+
+    expect(page).to_not have_content(@applicant_2.name)
+  end
+end
