e366b57e03138851315fe0f4e1e138c6b165ef02
Jacob Piland <piland.jacob@gmail.com>
2021-07-19 16:45:11 -0500

    refactor failing test

--- spec/models/adoption_application_spec.rb
+++ spec/models/adoption_application_spec.rb
@@ -13,6 +13,6 @@ RSpec.describe AdoptionApplication do
     it {should validate_presence_of(:state)}
     it {should validate_presence_of(:zip_code)}
     it {should validate_presence_of(:description)}
-    it {should validate_presence_of(:status)}
+  
   end
 end
